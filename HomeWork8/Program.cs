3f2118177214905aa1196f8ea6300b727d202bd9
Maksym Semenov <mestcomua@gmail.com>
2022-07-18 20:36:41 +0300

    111

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -7,16 +7,17 @@ namespace HomeWork8
         static void Main(string[] args)
         {
             List <Shape> Shapes = new List<Shape>();
-            Shapes.Add(new Circle("Trainee circle", 2));
-            Shapes.Add(new Circle("Junior circle", 3));
-            Shapes.Add(new Circle("Middle circle", 4));
-            Shapes.Add(new Circle("Senior circle", 5));
-            Shapes.Add(new Circle("TeamLead circle", 6));
-            Shapes.Add(new Square("Trainee square", 3));
-            Shapes.Add(new Square("Junior square", 4));
-            Shapes.Add(new Square("Middle square", 5));
-            Shapes.Add(new Square("Senior square", 6));
-            Shapes.Add(new Square("TeamLead square", 7));
+            //Shapes.Add(new Circle("Trainee circle", 2));
+            //Shapes.Add(new Circle("Junior circle", 3));
+            //Shapes.Add(new Circle("Middle circle", 4));
+            //Shapes.Add(new Circle("Senior circle", 5));
+            //Shapes.Add(new Circle("TeamLead circle", 6));
+            //Shapes.Add(new Square("Trainee square", 3));
+            //Shapes.Add(new Square("Junior square", 4));
+            //Shapes.Add(new Square("Middle square", 5));
+            //Shapes.Add(new Square("Senior square", 6));
+            //Shapes.Add(new Square("TeamLead square", 7));
+            Console.WriteLine("\tTask 1 - set list of Shapes");
             foreach (Shape s in Shapes)
             {
                 if (s.GetType() == typeof(Circle) || s.GetType() == typeof(Square))
